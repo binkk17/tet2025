955d5206f4a8298ef6f0a0f0dd4c54025ad4d32c
binkk17 <138805424+binkk17@users.noreply.github.com>
2025-01-25 00:12:48 +0700

    ngay14

---
+++ bai_tet2025/bai1.js
@@ -0,0 +1,14 @@
+// alert là một thông báo
+// Co 3 cach viet: truc tiep ngay trong the, trong the html(cai script ay), viet nhung tuong tu nhu viet css(thong dung hon vi 2 cach kia it dung)
+var button = document.querySelector("button");
+button.onclick = function() {
+alert("Hello World!");
+}
+
+// Cu phap: var tenBien= tenGiaTri
+// tab console la de debug js
+
+// Quy tac dat ten bien la phai bat dau bang chu cai hoac ky tu'_', khong bat dau bang so va cac ky tu dac biet
+// Cach viet hoa cac chu cai dau goi la:TenBien PascalCase
+// Cach viet: ten_bien la snake_case
+// Cach viet: tenBien la camelCase
