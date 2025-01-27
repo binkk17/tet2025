2e908fe5f997b2624a0928e8ee0a3cffe4d0f92b
binkk17 <138805424+binkk17@users.noreply.github.com>
2025-01-27 23:40:28 +0700

    ngay16

---
+++ bai_tet2025/bai3.js
@@ -0,0 +1,46 @@
+var a,b;
+a = prompt("Hay nhap so a");
+b = prompt("Hay nhap so b");
+
+// if(a>5){
+//     console.log("So dat yeu cau")
+// }
+// else{
+//     console.log("So nay chua dap ung yeu cau");
+//     a = prompt("Hay nhap lai so a");
+// }
+// while(a<=5 || b<=5){
+//     console.log("So chua dat yeu cau");
+//     a= prompt("Hay nhap lai so a");
+//     b= prompt("Hay nhap lai so b");
+// }
+// console.log("So da dat yeu cau");
+
+var result = a>=5 && b>=5 ? "Yes":"No";
+console.log(result);
+
+
+for(var i=0;i<=9;i++){
+    console.log(i);
+}
+
+var infor = {
+    fullname: "Nguyen van a",
+    age: 12,
+    quequan: "VinhLoc",
+}
+for(key in infor){
+    console.log(key);
+}
+for(key in infor){
+    console.log(key, infor[key]);
+}
+
+var ten =[
+    ten1 = "Ha van",
+    ten2 = "Nguye",
+    ten3 = "Havan",
+]
+for(key of ten){
+    console.log(key);
+}
