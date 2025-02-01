0d0f0971f8872db79398e9379c657cab1928f820
binkk17 <138805424+binkk17@users.noreply.github.com>
2025-02-01 23:37:23 +0700

    ngay18lan2

---
+++ bai_tet2025/javascript/bai5.js
@@ -0,0 +1,13 @@
+
+// day la sai cho 1 element
+const box = document.getElementById("box1");
+
+console.log(box);
+
+box.classList.add("dark");
+
+console.log(box);
+// lay ra thuoc tinh the
+const xxx = document.getElementsByTagName("h2");
+console.log(xxx);
+console.log(xxx[1].innerText);
